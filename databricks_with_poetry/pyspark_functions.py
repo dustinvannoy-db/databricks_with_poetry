7ac79f91a9b0cfd70806db85343e8114e0c969a2
dustinvannoy-db <dustin.vannoy@databricks.com>
2023-06-07 14:13:12 -0700

    Remove old function

--- databricks_with_poetry/pyspark_functions.py
+++ databricks_with_poetry/pyspark_functions.py
@@ -24,19 +24,3 @@ def prepare_spark():
         except ModuleNotFoundError as e:
             print("Not running on Databricks cluster and Databricks Connect not installed.")
             raise e
-
-# def get_spark_session():
-#     if os.getenv("DATABRICKS_RUNTIME_VERSION") is not None:
-#         from pyspark.sql import SparkSession
-#         return SparkSession.builder.getOrCreate()
-#     else:
-#         try:
-#             from databricks.connect import DatabricksSession
-#             from databricks.sdk.core import Config
-#             db_profile = os.getenv("DB_PROFILE", "field-eng")
-#             db_cluster = os.getenv("DB_CLUSTER")
-#             config = Config(profile=db_profile, cluster_id=db_cluster)
-#             return DatabricksSession.builder.sdkConfig(config).getOrCreate()
-#         except ModuleNotFoundError:
-#             from pyspark.sql import SparkSession
-#             return SparkSession.builder.getOrCreate()
