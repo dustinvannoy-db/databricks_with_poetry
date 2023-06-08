bb1ac74706927b79493e55ab44f776f4d10b3b90
dustinvannoy-db <dustinvannoy-db>
2023-06-08 18:38:47 +0000

    Update sample test

--- tests/unit/sample_test.py
+++ tests/unit/sample_test.py
@@ -1,6 +1,6 @@
 # from databricks_with_poetry.tasks.sample_etl_task import SampleETLTask
 # from databricks_with_poetry.tasks.sample_ml_task import SampleMLTask
-# from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession
 # from pathlib import Path
 # import mlflow
 # import logging
@@ -29,4 +29,41 @@
 #     assert runs.empty is False
 #     logging.info("Testing the ML job - done")
 
+import os
+print( os.getenv("DATABRICKS_RUNTIME_VERSION"))
+print(type(spark))
 
+def spark_session():
+    if os.getenv("DATABRICKS_RUNTIME_VERSION") is not None:
+        # return spark
+        return SparkSession.builder.getOrCreate()
+#     else:
+#         try:
+#             # from databricks.connect import DatabricksSession
+#             # from databricks.sdk.core import Config
+#             # config = Config(profile=db_profile, cluster_id=db_cluster)
+#             # return DatabricksSession.builder.sdkConfig(config).getOrCreate()
+#             from databricks.connect import DatabricksSession
+#             return DatabricksSession.builder.getOrCreate()
+#         except (ModuleNotFoundError, ValueError):
+#             from pyspark.sql import SparkSession
+#             return SparkSession.builder.master("local[*]").getOrCreate()
+
+print(type(spark_session()))
+
+def prepare_spark():
+    try:
+        from databricks.connect import DatabricksSession
+        return DatabricksSession.builder.getOrCreate()
+    except ModuleNotFoundError as e:
+        print("Databricks connect not installed")
+        try:
+            from pyspark.sql import SparkSession
+            return SparkSession.builder.getOrCreate()
+        except ModuleNotFoundError as e:
+            print("Databricks Connect not installed and pyspark not available.")
+            raise e
+print(type(prepare_spark()))
+
+from databricks.connect import DatabricksSession
+spark2 = DatabricksSession.builder.getOrCreate()
